08a2155141030dc28de4c95b6e39de532dca76ad
dlqbq <104298990+dlqbq@users.noreply.github.com>
2024-04-19 17:41:42 +0800

    Update random steps

--- main.py
+++ main.py
@@ -87,8 +87,8 @@ def main(_user, _passwd, _step):
         return "user and passwd not empty！"
 
     if _step == '':
-        print("已设置为随机步数（30000-39999）")
-        _step = str(random.randint(30000, 39999))
+        print("已设置为随机步数（10000-15000）")
+        _step = str(random.randint(10000, 15000))
     login_token, userid = login(_user, password)
     if login_token == 0:
         print("登陆失败！")
